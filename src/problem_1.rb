05c113eb5c71dcbe8ebd029984df8dca1279166c
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-06-11 22:39:38 +0900

    Add frozen literal

--- src/problem_1.rb
+++ src/problem_1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def sum_of_multiples_of_3_or_5
   (0..999).to_a.select { |i| (i % 3).zero? || (i % 5).zero? }.reduce(:+)
 end
--- test/test_problem_1.rb
+++ test/test_problem_1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 $LOAD_PATH << 'src'
 require 'minitest/autorun'
 require 'minitest/unit'
